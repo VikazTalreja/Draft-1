7bec5343e43ce3aece5c016aab7029f2063623f3
VikazTalreja <vikazztalreja@gmail.com>
2023-09-11 16:41:45 +0530

    Completed First Page

--- lib/pages/firstpage.dart
+++ lib/pages/firstpage.dart
@@ -8,23 +8,87 @@ class first extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(100.0),
+        preferredSize: const Size.fromHeight(150.0),
         child: AppBar(
           centerTitle: true,
-          title: const Padding(
-            padding: EdgeInsets.all(35.0),
-            child: Text(
-              "Textify",
-              style: TextStyle(
-                fontFamily: 'BerkshireSwashire',
-                color: Color.fromARGB(255, 124, 76, 212),
-                fontSize: 40,
+          title: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 40),
+            child: Container(
+              width: 300,
+              height: 100,
+              alignment: Alignment.center,
+              child: const Text(
+                "Textify",
+                style: TextStyle(
+                  fontFamily: 'BerkshireSwashire',
+                  color: Color.fromARGB(255, 66, 1, 187),
+                  fontSize: 40,
+                  letterSpacing: 5,
+                ),
               ),
             ),
           ),
           backgroundColor: Colors.white,
           elevation: 0.0,
         ),
+      ),
+      body: Column(
+        children: [
+          Image.asset('assets/images/Textify.jpg'),
+          const Text(
+            "We are What we do",
+            style: TextStyle(
+                fontFamily: 'AbhayaLibre',
+                fontStyle: FontStyle.normal,
+                fontSize: 35,
+                fontWeight: FontWeight.bold),
+          ),
+          const Text(
+            "Thousand of people are already Using Textify for summarizing",
+            style: TextStyle(
+              fontFamily: "AbhayaLibre",
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 50),
+            width: 350,
+            height: 50,
+            child: Container(
+              foregroundDecoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(10)),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(
+                        Color.fromARGB(255, 129, 77, 239))),
+                onPressed: () {},
+                child: const Text(
+                  "Sign Up",
+                  style: TextStyle(
+                    fontFamily: "AbhayaLibre",
+                    fontSize: 20,
+                    fontStyle: FontStyle.normal,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Row(
+            children: [
+              Text(
+                "Already have an Account?Log in",
+                style: TextStyle(
+                  fontFamily: 'AdhayaLibre',
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ],
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+          )
+        ],
       ),
     );
   }
